f4dd0c38d3bc24a37b592bb394b3992f06568667
Mario-PinedaD <mariopd2003@hotmail.com>
2025-04-24 21:35:08 -0600

    añadir estilos al html

--- headermain.php
+++ headermain.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/project.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cluster Habitacional</title>
 </head>
